ca177307a7a148960359c95c3e97f498fc08ee4f
SnehashisRatna <snehashisr10@gmail.com>
2024-04-25 16:15:50 +0530

    typecasting

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33" ; 
+
+console.log(score) ; 
+console.log(typeof(score)) ;
+
+
+ let value = Number(score);
+ console.log(value)
+console.log(typeof(value)) ;
+
+// "33" =>  33
+// "33abc" = >NaN -> Not a Number ; 
+// True = 1 
+// false = 0 ;
+
+// 1 => true  ;
+//"" => false ; 
+//"Sneha" => true  ;
+
