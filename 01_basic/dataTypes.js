84e5d376f9763fff94914a1b0e888ec0ca7a5230
SnehashisRatna <snehashisr10@gmail.com>
2024-04-25 15:19:10 +0530

    datatypes

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,10 @@
+"use strict" // Treat all js code as newer version  ;
+
+// alert(3+3)  ; This will throw a error beacause we  are using nodejs , Not Broswer ; 
+
+// Code readability should be high ; 
+
+
+
+ console.log(typeof undefined) ; 
+ console.log(typeof null) ; 
