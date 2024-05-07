87bab22d003e441c85f0faf3a24df2691dc08d63
SnehashisRatna <snehashisr10@gmail.com>
2024-05-07 12:22:36 +0530

    Learned Nesting Objects

--- 02-basic/03objects.js
+++ 02-basic/03objects.js
@@ -1,4 +1,11 @@
-// Objects Literals ; 
+// Objects Literals ;
+// Decartion of obejcts  
+
+// Singleton object
+ const userAcc = new Object()  ;
+
+// non singleton Objects  ; 
+
   const user = {
     name : "Sneha",
     age:18 , 
@@ -17,10 +24,26 @@
 // console.log(user) ;  
 
 // Function in object ; 
-
 //  In js we can treat function as a variables  ; 
 user.greeting = function(){
     console.log(`Hello ${this.name}`) ; // this keyword - > same object ko referecnce karne kiliye hum "this use karte hain;"
 }
 
 console.log(user.greeting()) ;
+
+//   Nesting Objects ; 
+
+const regularUser = {
+     id : 7 ,
+     fullName:{
+        userFullName :{
+            firstName : "Sneha" ,
+            lastName : "Ratna"
+        }
+     }
+}
+
+console.log(regularUser.fullName) ;
+console.log(regularUser.fullName.userFullName) ;
+console.log(regularUser.fullName.userFullName.firstName) ;
+console.log(regularUser.fullName.userFullName.lastName) ;
