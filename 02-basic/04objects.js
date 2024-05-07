68a90338f322b242e3782ed5baf1a2d3a75f81eb
SnehashisRatna <snehashisr10@gmail.com>
2024-05-07 13:12:18 +0530

    Object Destructing

---
+++ 02-basic/04objects.js
@@ -0,0 +1,16 @@
+// Objects Destructuring ; 
+const course = {
+    name : "js" ,
+    userName:"Sneha" ,
+    userId: "0007"  ,
+    price: "999" 
+}
+
+// destructuring :->>> 
+const  {userName :user} = course ; 
+
+     // console.log(userName) ;
+console.log(user) ; 
+
+// learned something about JSON and APi ; 
+// API -> Hotel mai jao  , order karoo aur kahke wapas ao .... but mera kaaam nhi hai udhar kahne banana ...this waht api do in backend ; 
